99f4dd3ae3658a317c5a688517ea39efc9651c1a
unknown <vincent.milano@soat.fr>
2019-04-09 10:45:16 +0200

    Minor refactos

--- src/heritage/shapes.spec.ts
+++ src/heritage/shapes.spec.ts
@@ -34,14 +34,15 @@ describe('Shapes', () => {
 
 
 
+/*
+    it('should still get rectangle area after width modification', () => {
+        r.setWidth(7);
+        r.getArea().should.equal(14);
+    });
 
-    // it('should still get rectangle area after width modification', () => {
-    //     r.setWidth(7);
-    //     r.getArea().should.equal(14);
-    // });
-    //
-    // it('should still get square area after width modification', () => {
-    //     s.setWidth(5);
-    //     s.getArea().should.equal(25);
-    // });
+    it('should still get square area after width modification', () => {
+        s.setWidth(5);
+        s.getArea().should.equal(25);
+    });
+*/
 });
